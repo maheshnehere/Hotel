959dc3bfeecbf80ad164223dc5f9a73dfb696fb6
Sourav2707 <smartsourav33@gmail.com>
2023-02-01 19:48:48 +0530

    Initial commit with welcome message to the hotel reservation system program

---
+++ src/main/java/com/bridgelabs/hotel_reservation_system/Main.java
@@ -0,0 +1,7 @@
+package com.bridgelabs.hotel_reservation_system;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Hotel Reservation System program");
+    }
+}
