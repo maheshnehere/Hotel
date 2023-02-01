005c7bc9c4216695b3e587438a670768b6059079
Sourav2707 <smartsourav33@gmail.com>
2023-02-01 20:30:27 +0530

    UC1 Ability to add Hotel in a Hotel Reservation System with Name and rates for Regular Customer

---
+++ src/test/java/com/bridgelabs/hotel_reservation_system/HotelReservationSystemTest.java
@@ -0,0 +1,16 @@
+package com.bridgelabs.hotel_reservation_system;
+
+import org.junit.jupiter.api.Assertions;
+
+import static org.junit.jupiter.api.Assertions.*;
+class HotelReservationSystemTest {
+
+    @org.junit.jupiter.api.Test
+    void uc1_addHotel() {
+        String expected = "Name: LakeWood Rate: 110";
+        HotelReservationSystem hotelReservationSystem = new HotelReservationSystem();
+        hotelReservationSystem.uc1_addHotel("LakeWood", 110);
+        String actual = hotelReservationSystem.hotels.get(0).toString();
+        Assertions.assertEquals(expected, actual);
+    }
+}
